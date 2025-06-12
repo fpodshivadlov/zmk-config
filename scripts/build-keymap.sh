8badee422b9db476f45f713dbcac350d956a057c
Fyodor Podshivadlov <email@fyodorpodshivadlov.com>
2025-06-12 12:02:49 +0300

    feat: update keymap and keymap drawer config

--- scripts/build-keymap.sh
+++ scripts/build-keymap.sh
@@ -6,7 +6,6 @@ output_dir="$source_dir/keymap-drawer"
 mkdir -p "$output_dir"
 
 keymap_config=${source_dir}/keymap_drawer.config.yaml
-export KEYMAP_dark_mode=auto
 
 keymap -c ${keymap_config} parse -c 10 -z "$source_dir/config/eyelash_corne.keymap" > "$output_dir/eyelash_corne.yaml"
 keymap -c ${keymap_config} draw "$output_dir/eyelash_corne.yaml" -d "$source_dir/boards/arm/eyelash_corne/eyelash_corne-layouts.dtsi" > "$output_dir/eyelash_corne.svg"
