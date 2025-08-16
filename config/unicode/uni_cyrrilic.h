42a29cbaf73b2fe8040fe5038c85e04995833590
Fyodor Podshivadlov <email@fyodorpodshivadlov.com>
2025-08-16 08:51:18 +0000

    feat: add Cyrillic unicode definitions and include necessary headers

---
+++ config/unicode/uni_cyrrilic.h
@@ -0,0 +1,1033 @@
+// Cyrillic extensions
+// Ѐ
+// Cyrillic Capital Letter Ie with Grave
+// Ё
+// Cyrillic Capital Letter Io
+// Ђ
+// Cyrillic Capital Letter Dje
+// Ѓ
+// Cyrillic Capital Letter Gje
+// Є
+// Cyrillic Capital Letter Ukrainian Ie
+// Ѕ
+// Cyrillic Capital Letter Dze
+// І
+// Cyrillic Capital Letter Byelorussian-Ukrainian I
+// Ї
+// Cyrillic Capital Letter Yi
+// Ј
+// Cyrillic Capital Letter Je
+// Љ
+// Cyrillic Capital Letter Lje
+// Њ
+// Cyrillic Capital Letter Nje
+// Ћ
+// Cyrillic Capital Letter Tshe
+// Ќ
+// Cyrillic Capital Letter Kje
+// Ѝ
+// Cyrillic Capital Letter I with Grave
+// Ў
+// Cyrillic Capital Letter Short U
+// Џ
+// Cyrillic Capital Letter Dzhe
+// Basic Russian alphabet
+// А
+// Cyrillic Capital Letter A
+// Б
+// Cyrillic Capital Letter Be
+// В
+// Cyrillic Capital Letter Ve
+// Г
+// Cyrillic Capital Letter Ghe
+// Д
+// Cyrillic Capital Letter De
+// Е
+// Cyrillic Capital Letter Ie
+// Ж
+// Cyrillic Capital Letter Zhe
+// З
+// Cyrillic Capital Letter Ze
+// И
+// Cyrillic Capital Letter I
+// Й
+// Cyrillic Capital Letter Short I
+// К
+// Cyrillic Capital Letter Ka
+// Л
+// Cyrillic Capital Letter El
+// М
+// Cyrillic Capital Letter Em
+// Н
+// Cyrillic Capital Letter En
+// О
+// Cyrillic Capital Letter O
+// П
+// Cyrillic Capital Letter Pe
+// Р
+// Cyrillic Capital Letter Er
+// С
+// Cyrillic Capital Letter Es
+// Т
+// Cyrillic Capital Letter Te
+// У
+// Cyrillic Capital Letter U
+// Ф
+// Cyrillic Capital Letter Ef
+// Х
+// Cyrillic Capital Letter Ha
+// Ц
+// Cyrillic Capital Letter Tse
+// Ч
+// Cyrillic Capital Letter Che
+// Ш
+// Cyrillic Capital Letter Sha
+// Щ
+// Cyrillic Capital Letter Shcha
+// Ъ
+// Cyrillic Capital Letter Hard Sign
+// Ы
+// Cyrillic Capital Letter Yeru
+// Ь
+// Cyrillic Capital Letter Soft Sign
+// Э
+// Cyrillic Capital Letter E
+// Ю
+// Cyrillic Capital Letter Yu
+// Я
+// Cyrillic Capital Letter Ya
+// а
+// Cyrillic Small Letter A
+// б
+// Cyrillic Small Letter Be
+// в
+// Cyrillic Small Letter Ve
+// г
+// Cyrillic Small Letter Ghe
+// д
+// Cyrillic Small Letter De
+// е
+// Cyrillic Small Letter Ie
+// ж
+// Cyrillic Small Letter Zhe
+// з
+// Cyrillic Small Letter Ze
+// и
+// Cyrillic Small Letter I
+// й
+// Cyrillic Small Letter Short I
+// к
+// Cyrillic Small Letter Ka
+// л
+// Cyrillic Small Letter El
+// м
+// Cyrillic Small Letter Em
+// н
+// Cyrillic Small Letter En
+// о
+// Cyrillic Small Letter O
+// п
+// Cyrillic Small Letter Pe
+// р
+// Cyrillic Small Letter Er
+// с
+// Cyrillic Small Letter Es
+// т
+// Cyrillic Small Letter Te
+// у
+// Cyrillic Small Letter U
+// ф
+// Cyrillic Small Letter Ef
+// х
+// Cyrillic Small Letter Ha
+// ц
+// Cyrillic Small Letter Tse
+// ч
+// Cyrillic Small Letter Che
+// ш
+// Cyrillic Small Letter Sha
+// щ
+// Cyrillic Small Letter Shcha
+// ъ
+// Cyrillic Small Letter Hard Sign
+// ы
+// Cyrillic Small Letter Yeru
+// ь
+// Cyrillic Small Letter Soft Sign
+// э
+// Cyrillic Small Letter E
+// ю
+// Cyrillic Small Letter Yu
+// я
+// Cyrillic Small Letter Ya
+// Cyrillic extensions
+// ѐ
+// Cyrillic Small Letter Ie with Grave
+// ё
+// Cyrillic Small Letter Io
+// ђ
+// Cyrillic Small Letter Dje
+// ѓ
+// Cyrillic Small Letter Gje
+// є
+// Cyrillic Small Letter Ukrainian Ie
+// ѕ
+// Cyrillic Small Letter Dze
+// і
+// Cyrillic Small Letter Byelorussian-Ukrainian I
+// ї
+// Cyrillic Small Letter Yi
+// ј
+// Cyrillic Small Letter Je
+// љ
+// Cyrillic Small Letter Lje
+// њ
+// Cyrillic Small Letter Nje
+// ћ
+// Cyrillic Small Letter Tshe
+// ќ
+// Cyrillic Small Letter Kje
+// ѝ
+// Cyrillic Small Letter I with Grave
+// ў
+// Cyrillic Small Letter Short U
+// џ
+// Cyrillic Small Letter Dzhe
+// Historic letters
+// Ѡ
+// Cyrillic Capital Letter Omega
+// ѡ
+// Cyrillic Small Letter Omega
+// Ѣ
+// Cyrillic Capital Letter Yat
+// ѣ
+// Cyrillic Small Letter Yat
+// Ѥ
+// Cyrillic Capital Letter Iotified E
+// ѥ
+// Cyrillic Small Letter Iotified E
+// Ѧ
+// Cyrillic Capital Letter Little Yus
+// ѧ
+// Cyrillic Small Letter Little Yus
+// Ѩ
+// Cyrillic Capital Letter Iotified Little Yus
+// ѩ
+// Cyrillic Small Letter Iotified Little Yus
+// Ѫ
+// Cyrillic Capital Letter Big Yus
+// ѫ
+// Cyrillic Small Letter Big Yus
+// Ѭ
+// Cyrillic Capital Letter Iotified Big Yus
+// ѭ
+// Cyrillic Small Letter Iotified Big Yus
+// Ѯ
+// Cyrillic Capital Letter Ksi
+// ѯ
+// Cyrillic Small Letter Ksi
+// Ѱ
+// Cyrillic Capital Letter Psi
+// ѱ
+// Cyrillic Small Letter Psi
+// Ѳ
+// Cyrillic Capital Letter Fita
+// ѳ
+// Cyrillic Small Letter Fita
+// Ѵ
+// Cyrillic Capital Letter Izhitsa
+// ѵ
+// Cyrillic Small Letter Izhitsa
+// Ѷ
+// Cyrillic Capital Letter Izhitsa with Double Grave Accent
+// ѷ
+// Cyrillic Small Letter Izhitsa with Double Grave Accent
+// Historic digraphs
+// Ѹ
+// Cyrillic Capital Letter Uk
+// ѹ
+// Cyrillic Small Letter Uk
+// Historic letters
+// Ѻ
+// Cyrillic Capital Letter Round Omega
+// ѻ
+// Cyrillic Small Letter Round Omega
+// Ѽ
+// Cyrillic Capital Letter Omega with Titlo
+// ѽ
+// Cyrillic Small Letter Omega with Titlo
+// Ѿ
+// Cyrillic Capital Letter Ot
+// ѿ
+// Cyrillic Small Letter Ot
+// Ҁ
+// Cyrillic Capital Letter Koppa
+// ҁ
+// Cyrillic Small Letter Koppa
+// Historic miscellaneous
+// ҂
+// Cyrillic Thousands Sign
+// ҃
+// Combining Cyrillic Titlo
+// ҄
+// Combining Cyrillic Palatalization
+// ҅
+// Combining Cyrillic Dasia Pneumata
+// ҆
+// Combining Cyrillic Psili Pneumata
+// ҇
+// Combining Cyrillic Pokrytie
+// ҈
+// Combining Cyrillic Hundred Thousands Sign
+// ҉
+// Combining Cyrillic Millions Sign
+// Extended Cyrillic
+// Ҋ
+// Cyrillic Capital Letter Short I with Tail
+// ҋ
+// Cyrillic Small Letter Short I with Tail
+// Ҍ
+// Cyrillic Capital Letter Semisoft Sign
+// ҍ
+// Cyrillic Small Letter Semisoft Sign
+// Ҏ
+// Cyrillic Capital Letter Er with Tick
+// ҏ
+// Cyrillic Small Letter Er with Tick
+// Ґ
+// Cyrillic Capital Letter Ghe with Upturn
+// ґ
+// Cyrillic Small Letter Ghe with Upturn
+// Ғ
+// Cyrillic Capital Letter Ghe with Stroke
+// ғ
+// Cyrillic Small Letter Ghe with Stroke
+// Ҕ
+// Cyrillic Capital Letter Ghe with Middle Hook
+// ҕ
+// Cyrillic Small Letter Ghe with Middle Hook
+// Җ
+// Cyrillic Capital Letter Zhe with Descender
+// җ
+// Cyrillic Small Letter Zhe with Descender
+// Ҙ
+// Cyrillic Capital Letter Ze with Descender
+// ҙ
+// Cyrillic Small Letter Ze with Descender
+// Қ
+// Cyrillic Capital Letter Ka with Descender
+// қ
+// Cyrillic Small Letter Ka with Descender
+// Ҝ
+// Cyrillic Capital Letter Ka with Vertical Stroke
+// ҝ
+// Cyrillic Small Letter Ka with Vertical Stroke
+// Ҟ
+// Cyrillic Capital Letter Ka with Stroke
+// ҟ
+// Cyrillic Small Letter Ka with Stroke
+// Ҡ
+// Cyrillic Capital Letter Bashkir Ka
+// ҡ
+// Cyrillic Small Letter Bashkir Ka
+// Ң
+// Cyrillic Capital Letter En with Descender
+// ң
+// Cyrillic Small Letter En with Descender
+// Ҥ
+// Cyrillic Capital Ligature En Ghe
+// ҥ
+// Cyrillic Small Ligature En Ghe
+// Ҧ
+// Cyrillic Capital Letter Pe with Middle Hook
+// ҧ
+// Cyrillic Small Letter Pe with Middle Hook
+// Ҩ
+// Cyrillic Capital Letter Abkhasian Ha
+// ҩ
+// Cyrillic Small Letter Abkhasian Ha
+// Ҫ
+// Cyrillic Capital Letter Es with Descender
+// ҫ
+// Cyrillic Small Letter Es with Descender
+// Ҭ
+// Cyrillic Capital Letter Te with Descender
+// ҭ
+// Cyrillic Small Letter Te with Descender
+// Ү
+// Cyrillic Capital Letter Straight U
+// ү
+// Cyrillic Small Letter Straight U
+// Ұ
+// Cyrillic Capital Letter Straight U with Stroke
+// ұ
+// Cyrillic Small Letter Straight U with Stroke
+// Ҳ
+// Cyrillic Capital Letter Ha with Descender
+// ҳ
+// Cyrillic Small Letter Ha with Descender
+// Ҵ
+// Cyrillic Capital Ligature Te Tse
+// ҵ
+// Cyrillic Small Ligature Te Tse
+// Ҷ
+// Cyrillic Capital Letter Che with Descender
+// ҷ
+// Cyrillic Small Letter Che with Descender
+// Ҹ
+// Cyrillic Capital Letter Che with Vertical Stroke
+// ҹ
+// Cyrillic Small Letter Che with Vertical Stroke
+// Һ
+// Cyrillic Capital Letter Shha
+// һ
+// Cyrillic Small Letter Shha
+// Ҽ
+// Cyrillic Capital Letter Abkhasian Che
+// ҽ
+// Cyrillic Small Letter Abkhasian Che
+// Ҿ
+// Cyrillic Capital Letter Abkhasian Che with Descender
+// ҿ
+// Cyrillic Small Letter Abkhasian Che with Descender
+// Ӏ
+// Cyrillic Letter Palochka
+// Ӂ
+// Cyrillic Capital Letter Zhe with Breve
+// ӂ
+// Cyrillic Small Letter Zhe with Breve
+// Ӄ
+// Cyrillic Capital Letter Ka with Hook
+// ӄ
+// Cyrillic Small Letter Ka with Hook
+// Ӆ
+// Cyrillic Capital Letter El with Tail
+// ӆ
+// Cyrillic Small Letter El with Tail
+// Ӈ
+// Cyrillic Capital Letter En with Hook
+// ӈ
+// Cyrillic Small Letter En with Hook
+// Ӊ
+// Cyrillic Capital Letter En with Tail
+// ӊ
+// Cyrillic Small Letter En with Tail
+// Ӌ
+// Cyrillic Capital Letter Khakassian Che
+// ӌ
+// Cyrillic Small Letter Khakassian Che
+// Ӎ
+// Cyrillic Capital Letter Em with Tail
+// ӎ
+// Cyrillic Small Letter Em with Tail
+// ӏ
+// Cyrillic Small Letter Palochka
+// Ӑ
+// Cyrillic Capital Letter a with Breve
+// ӑ
+// Cyrillic Small Letter a with Breve
+// Ӓ
+// Cyrillic Capital Letter a with Diaeresis
+// ӓ
+// Cyrillic Small Letter a with Diaeresis
+// Ӕ
+// Cyrillic Capital Ligature a Ie
+// ӕ
+// Cyrillic Small Ligature a Ie
+// Ӗ
+// Cyrillic Capital Letter Ie with Breve
+// ӗ
+// Cyrillic Small Letter Ie with Breve
+// Ә
+// Cyrillic Capital Letter Schwa
+// ә
+// Cyrillic Small Letter Schwa
+// Ӛ
+// Cyrillic Capital Letter Schwa with Diaeresis
+// ӛ
+// Cyrillic Small Letter Schwa with Diaeresis
+// Ӝ
+// Cyrillic Capital Letter Zhe with Diaeresis
+// ӝ
+// Cyrillic Small Letter Zhe with Diaeresis
+// Ӟ
+// Cyrillic Capital Letter Ze with Diaeresis
+// ӟ
+// Cyrillic Small Letter Ze with Diaeresis
+// Ӡ
+// Cyrillic Capital Letter Abkhasian Dze
+// ӡ
+// Cyrillic Small Letter Abkhasian Dze
+// Ӣ
+// Cyrillic Capital Letter I with Macron
+// ӣ
+// Cyrillic Small Letter I with Macron
+// Ӥ
+// Cyrillic Capital Letter I with Diaeresis
+// ӥ
+// Cyrillic Small Letter I with Diaeresis
+// Ӧ
+// Cyrillic Capital Letter O with Diaeresis
+// ӧ
+// Cyrillic Small Letter O with Diaeresis
+// Ө
+// Cyrillic Capital Letter Barred O
+// ө
+// Cyrillic Small Letter Barred O
+// Ӫ
+// Cyrillic Capital Letter Barred O with Diaeresis
+// ӫ
+// Cyrillic Small Letter Barred O with Diaeresis
+// Ӭ
+// Cyrillic Capital Letter E with Diaeresis
+// ӭ
+// Cyrillic Small Letter E with Diaeresis
+// Ӯ
+// Cyrillic Capital Letter U with Macron
+// ӯ
+// Cyrillic Small Letter U with Macron
+// Ӱ
+// Cyrillic Capital Letter U with Diaeresis
+// ӱ
+// Cyrillic Small Letter U with Diaeresis
+// Ӳ
+// Cyrillic Capital Letter U with Double Acute
+// ӳ
+// Cyrillic Small Letter U with Double Acute
+// Ӵ
+// Cyrillic Capital Letter Che with Diaeresis
+// ӵ
+// Cyrillic Small Letter Che with Diaeresis
+// Ӷ
+// Cyrillic Capital Letter Ghe with Descender
+// ӷ
+// Cyrillic Small Letter Ghe with Descender
+// Ӹ
+// Cyrillic Capital Letter Yeru with Diaeresis
+// ӹ
+// Cyrillic Small Letter Yeru with Diaeresis
+// Additions for Nivkh
+// Ӻ
+// Cyrillic Capital Letter Ghe with Stroke and Hook
+// ӻ
+// Cyrillic Small Letter Ghe with Stroke and Hook
+// Ӽ
+// Cyrillic Capital Letter Ha with Hook
+// ӽ
+// Cyrillic Small Letter Ha with Hook
+// Ӿ
+// Cyrillic Capital Letter Ha with Stroke
+// ӿ
+// Cyrillic Small Letter Ha with Stroke
+// Ѐ
+// 0400
+// Ё
+// 0401
+// Ђ
+// 0402
+// Ѓ
+// 0403
+// Є
+// 0404
+// Ѕ
+// 0405
+// І
+// 0406
+// Ї
+// 0407
+// Ј
+// 0408
+// Љ
+// 0409
+// Њ
+// 040A
+// Ћ
+// 040B
+// Ќ
+// 040C
+// Ѝ
+// 040D
+// Ў
+// 040E
+// Џ
+// 040F
+// А
+// 0410
+// Б
+// 0411
+// В
+// 0412
+// Г
+// 0413
+// Д
+// 0414
+// Е
+// 0415
+// Ж
+// 0416
+// З
+// 0417
+// И
+// 0418
+// Й
+// 0419
+// К
+// 041A
+// Л
+// 041B
+// М
+// 041C
+// Н
+// 041D
+// О
+// 041E
+// П
+// 041F
+// Р
+// 0420
+// С
+// 0421
+// Т
+// 0422
+// У
+// 0423
+// Ф
+// 0424
+// Х
+// 0425
+// Ц
+// 0426
+// Ч
+// 0427
+// Ш
+// 0428
+// Щ
+// 0429
+// Ъ
+// 042A
+// Ы
+// 042B
+// Ь
+// 042C
+// Э
+// 042D
+// Ю
+// 042E
+// Я
+// 042F
+// а
+// 0430
+// б
+// 0431
+// в
+// 0432
+// г
+// 0433
+// д
+// 0434
+// е
+// 0435
+// ж
+// 0436
+// з
+// 0437
+// и
+// 0438
+// й
+// 0439
+// к
+// 043A
+// л
+// 043B
+// м
+// 043C
+// н
+// 043D
+// о
+// 043E
+// п
+// 043F
+// р
+// 0440
+// с
+// 0441
+// т
+// 0442
+// у
+// 0443
+// ф
+// 0444
+// х
+// 0445
+// ц
+// 0446
+// ч
+// 0447
+// ш
+// 0448
+// щ
+// 0449
+// ъ
+// 044A
+// ы
+// 044B
+// ь
+// 044C
+// э
+// 044D
+// ю
+// 044E
+// я
+// 044F
+// ѐ
+// 0450
+// ё
+// 0451
+// ђ
+// 0452
+// ѓ
+// 0453
+// є
+// 0454
+// ѕ
+// 0455
+// і
+// 0456
+// ї
+// 0457
+// ј
+// 0458
+// љ
+// 0459
+// њ
+// 045A
+// ћ
+// 045B
+// ќ
+// 045C
+// ѝ
+// 045D
+// ў
+// 045E
+// џ
+// 045F
+// Ѡ
+// 0460
+// ѡ
+// 0461
+// Ѣ
+// 0462
+// ѣ
+// 0463
+// Ѥ
+// 0464
+// ѥ
+// 0465
+// Ѧ
+// 0466
+// ѧ
+// 0467
+// Ѩ
+// 0468
+// ѩ
+// 0469
+// Ѫ
+// 046A
+// ѫ
+// 046B
+// Ѭ
+// 046C
+// ѭ
+// 046D
+// Ѯ
+// 046E
+// ѯ
+// 046F
+// Ѱ
+// 0470
+// ѱ
+// 0471
+// Ѳ
+// 0472
+// ѳ
+// 0473
+// Ѵ
+// 0474
+// ѵ
+// 0475
+// Ѷ
+// 0476
+// ѷ
+// 0477
+// Ѹ
+// 0478
+// ѹ
+// 0479
+// Ѻ
+// 047A
+// ѻ
+// 047B
+// Ѽ
+// 047C
+// ѽ
+// 047D
+// Ѿ
+// 047E
+// ѿ
+// 047F
+// Ҁ
+// 0480
+// ҁ
+// 0481
+// ҂
+// 0482
+// ҃
+// 0483
+// ҄
+// 0484
+// ҅
+// 0485
+// ҆
+// 0486
+// ҇
+// 0487
+// ҈
+// 0488
+// ҉
+// 0489
+// Ҋ
+// 048A
+// ҋ
+// 048B
+// Ҍ
+// 048C
+// ҍ
+// 048D
+// Ҏ
+// 048E
+// ҏ
+// 048F
+// Ґ
+// 0490
+// ґ
+// 0491
+// Ғ
+// 0492
+// ғ
+// 0493
+// Ҕ
+// 0494
+// ҕ
+// 0495
+// Җ
+// 0496
+// җ
+// 0497
+// Ҙ
+// 0498
+// ҙ
+// 0499
+// Қ
+// 049A
+// қ
+// 049B
+// Ҝ
+// 049C
+// ҝ
+// 049D
+// Ҟ
+// 049E
+// ҟ
+// 049F
+// Ҡ
+// 04A0
+// ҡ
+// 04A1
+// Ң
+// 04A2
+// ң
+// 04A3
+// Ҥ
+// 04A4
+// ҥ
+// 04A5
+// Ҧ
+// 04A6
+// ҧ
+// 04A7
+// Ҩ
+// 04A8
+// ҩ
+// 04A9
+// Ҫ
+// 04AA
+// ҫ
+// 04AB
+// Ҭ
+// 04AC
+// ҭ
+// 04AD
+// Ү
+// 04AE
+// ү
+// 04AF
+// Ұ
+// 04B0
+// ұ
+// 04B1
+// Ҳ
+// 04B2
+// ҳ
+// 04B3
+// Ҵ
+// 04B4
+// ҵ
+// 04B5
+// Ҷ
+// 04B6
+// ҷ
+// 04B7
+// Ҹ
+// 04B8
+// ҹ
+// 04B9
+// Һ
+// 04BA
+// һ
+// 04BB
+// Ҽ
+// 04BC
+// ҽ
+// 04BD
+// Ҿ
+// 04BE
+// ҿ
+// 04BF
+// Ӏ
+// 04C0
+// Ӂ
+// 04C1
+// ӂ
+// 04C2
+// Ӄ
+// 04C3
+// ӄ
+// 04C4
+// Ӆ
+// 04C5
+// ӆ
+// 04C6
+// Ӈ
+// 04C7
+// ӈ
+// 04C8
+// Ӊ
+// 04C9
+// ӊ
+// 04CA
+// Ӌ
+// 04CB
+// ӌ
+// 04CC
+// Ӎ
+// 04CD
+// ӎ
+// 04CE
+// ӏ
+// 04CF
+// Ӑ
+// 04D0
+// ӑ
+// 04D1
+// Ӓ
+// 04D2
+// ӓ
+// 04D3
+// Ӕ
+// 04D4
+// ӕ
+// 04D5
+// Ӗ
+// 04D6
+// ӗ
+// 04D7
+// Ә
+// 04D8
+// ә
+// 04D9
+// Ӛ
+// 04DA
+// ӛ
+// 04DB
+// Ӝ
+// 04DC
+// ӝ
+// 04DD
+// Ӟ
+// 04DE
+// ӟ
+// 04DF
+// Ӡ
+// 04E0
+// ӡ
+// 04E1
+// Ӣ
+// 04E2
+// ӣ
+// 04E3
+// Ӥ
+// 04E4
+// ӥ
+// 04E5
+// Ӧ
+// 04E6
+// ӧ
+// 04E7
+// Ө
+// 04E8
+// ө
+// 04E9
+// Ӫ
+// 04EA
+// ӫ
+// 04EB
+// Ӭ
+// 04EC
+// ӭ
+// 04ED
+// Ӯ
+// 04EE
+// ӯ
+// 04EF
+// Ӱ
+// 04F0
+// ӱ
+// 04F1
+// Ӳ
+// 04F2
+// ӳ
+// 04F3
+// Ӵ
+// 04F4
+// ӵ
+// 04F5
+// Ӷ
+// 04F6
+// ӷ
+// 04F7
+// Ӹ
+// 04F8
+// ӹ
+// 04F9
+// Ӻ
+// 04FA
+// ӻ
+// 04FB
+// Ӽ
+// 04FC
+// ӽ
+// 04FD
+// Ӿ
+// 04FE
+// ӿ
+// 04FF
